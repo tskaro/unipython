66a5db75dd24a04ace051846584337d466b0026c
tskaro <tskaro@gmail.com>
2021-01-12 00:48:01 +0400

    have defined new 'menu' function to print all products of the port

--- middle_project/middle_project.py
+++ middle_project/middle_project.py
@@ -15,20 +15,26 @@ class craftport:
         self.address = address
         self.products = products
 
+    def menu(self):
+        print('Welcome in Foundations intergalactic port, here is our legendary assortment:')
+        for product in self.products:
+            print(f'{product.name} with light speed of {product.speed}, weapon on a bord rank {product.arm},'
+                  f' total capicity of {product.capacity} people. We have {product.quantity} left of those')
+
 
 # spacecraft list---------------------------------
 
 foundations_intergalactic_port = craftport("Foundations intergalactic port", "Canis Major Overdensity", [])
-#products = []
 craft_list = [
-    {"name": "Jedi Starfighter", "speed": 5, "arm": 1, "capacity": 15, "quantity": 11},
+    {"name": "Jedi Starfighter", "speed": 1, "arm": 1, "capacity": 15, "quantity": 11},
     {"name": "Starship Enterprise", "speed": 15, "arm": 9, "capacity": 3000, "quantity": 6},
-    {"name": "Millennium Falcon", "speed": 18, "arm": 10, "capacity": 4500, "quantity": 3},
-    {"name": "Millennium Falcon", "speed": 18, "arm": 10, "capacity": 4500, "quantity": 1},
+    {"name": "Millennium Falcon", "speed": 18, "arm": 10, "capacity": 10, "quantity": 8},
+    {"name": "super galaxy gurren lagann", "speed": 30, "arm": 10, "capacity": 10000, "quantity": 1}
 ]
 
 for item in craft_list:
     name, speed, arm, capacity, quantity = item.values()
     foundations_intergalactic_port.products.append(spacecraft(name, speed, arm, capacity, quantity))
 
-print(foundations_intergalactic_port.products[1].arm)
+#print(foundations_intergalactic_port.products[1].arm)
+print(foundations_intergalactic_port.menu())
