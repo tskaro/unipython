59a0df7d36717928b10b80a4f63621cc0227e13d
tskaro <tskaro@gmail.com>
2021-02-09 21:52:39 +0400

    populated ship db

--- Flask_projects/Flask_JWT_SQlite/populate.py
+++ Flask_projects/Flask_JWT_SQlite/populate.py
@@ -1,20 +1,24 @@
 import sqlite3
 
-user_list = [
-    (1, "tskaro", "unilab21"),
-    (2, "user2", "unilab22"),
-    (3, "user3", "unilab23")
+items = [
+    {"id": 1, "name": "Jedi Starfighter", "speed": 1, "arm": 1, "capacity": 15, "quantity": 11},
+    {"id": 2, "name": "Starship Enterprise", "speed": 15, "arm": 9, "capacity": 3000, "quantity": 6},
+    {"id": 3, "name": "Millennium Falcon", "speed": 18, "arm": 10, "capacity": 10, "quantity": 8},
+    {"id": 4, "name": "super galaxy gurren lagann", "speed": 30, "arm": 10, "capacity": 10000, "quantity": 1}
 ]
+list = []
+for item in items:
+    (*values,) = item.values()
+    list.append((*values,))
 
 connection = sqlite3.connect("data.db")
-
 cursor = connection.cursor()
 
-cursor.execute('CREATE TABLE IF NOT EXISTS users (user_id INTEGER PRIMARY KEY, username text, password text)')
+cursor.execute('CREATE TABLE IF NOT EXISTS ships (id INTEGER PRIMARY KEY, name text, speed integer, arm integer, capacity integer, quantity integer)')
 
-query_string = "INSERT INTO users VALUES(?, ?, ?)"
+query_string = "INSERT INTO ships VALUES(?, ?, ?, ?, ?, ?)"
 
-cursor.executemany(query_string, user_list)
+cursor.executemany(query_string, list)
 
 connection.commit()
 
